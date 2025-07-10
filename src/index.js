ecb670b9dce549b87b9c5c27ece8395aca73c0d3
정범수 <jbss0708@gmail.com>
2025-07-10 21:28:29 +0900

    [Fix] 문구수정, 지도수정

---
+++ src/index.js
@@ -0,0 +1,74 @@
+// index.js
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import "./styles/WeddingPage.css";
+import WeddingPage from './views/WeddingPage.js';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import GalleryPage from './views/GalleryPage.js';
+import Modal from 'react-modal';
+import KakaoShare from './components/KakaoShare'; // KakaoShare 컴포넌트 임포트
+
+Modal.setAppElement('#root');
+
+const Root = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<WeddingPage />} />
+      <Route path="/gallery" element={<GalleryPage />} />
+      <Route path="/share" element={<KakaoShare />} />
+    </Routes>
+  );
+};
+
+// Select the root DOM node
+const container = document.getElementById('root');
+
+// Create a root.
+const root = createRoot(container);
+
+// Kakao SDK 로드 및 초기화 함수
+const loadKakaoSDK = () => {
+  return new Promise((resolve, reject) => {
+    if (window.Kakao) {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init('9231a409e3ef488e46f59b7a5f3f787c');
+      }
+      resolve();
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.src = 'https://developers.kakao.com/sdk/js/kakao.min.js';
+    // script.src = 'https://developers.kakao.com/sdk/js/kakao.js';    
+    script.async = true;
+    script.onload = () => {
+      if (window.Kakao) {
+        window.Kakao.init('9231a409e3ef488e46f59b7a5f3f787c');
+        resolve();
+      } else {
+        reject(new Error('Kakao SDK 로드 실패'));
+      }
+    };
+    script.onerror = () => reject(new Error('Kakao SDK 로드 실패'));
+    document.head.appendChild(script);
+  });
+};
+
+// Kakao SDK 로드 후 애플리케이션 렌더링
+loadKakaoSDK().then(() => {
+  console.log('Kakao SDK 초기화 완료');
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <Root />
+      </Router>
+    </React.StrictMode>
+  );
+}).catch((error) => {
+  console.error(error);
+});
